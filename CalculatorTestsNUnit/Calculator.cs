1319bb17cf1453892d4fbafaae6bedd9acf5a2c9
stagiaire <stagiaire@DESKTOP-DR9J1N7>
2022-10-13 10:27:38 +0200

    Ajoutez des fichiers projet.

---
+++ CalculatorTestsNUnit/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculatorTestsNUnit
+{
+    public static class Calculator
+    {
+
+        public static int Add(string numbers)
+        {
+            if (numbers == null || numbers == "")
+                return 0;
+
+            var arrayNumbers = numbers.Split(',','\n');
+
+            var result = arrayNumbers.Where(number => Convert.ToInt32(number)<=1000).Sum(sumNumber => Convert.ToInt32(sumNumber));
+            return result;
+        }
+    }
+}
